70923ac5ee5e26264fd7d24d1754dd1b7fad0e52
chrishatherall <chris@pixelnebula.com>
2021-08-16 14:51:41 +0100

    Fixed spawned items disappearing when host leaves

--- Assets/Scripts/ItemSpawn.cs
+++ Assets/Scripts/ItemSpawn.cs
@@ -21,6 +21,6 @@ public class ItemSpawn : MonoBehaviour
         // Find a random item from our list
         GameObject prefab = gm.GetItemFromSpawnList(spawnList);
         lm.Log(logSrc,"Spawning item: " + prefab.name);
-        PhotonNetwork.Instantiate(prefab.name, this.transform.position, this.transform.rotation, 0);
+        PhotonNetwork.InstantiateSceneObject(prefab.name, this.transform.position, this.transform.rotation, 0);
     }
 }
